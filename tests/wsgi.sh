ead0efedfb7c4c208a926b01947e135dfcd9df80
kirimaks <kirimaks@yahoo.com>
2016-02-20 15:52:33 +0000

    write tests

--- tests/wsgi.sh
+++ tests/wsgi.sh
@@ -1,3 +1,3 @@
 curl "http://0.0.0.0:8080//?a=1&a=2&b=3" 	#-vv
-curl "http://127.0.0.1:8080//?a=1&a=2&b=3" 	#-vv
-curl "http://127.0.0.1/hello/" 			#-vv
+curl "http://127.0.0.1:8080/?a=1&a=2&b=3" 	#-vv
+curl "http://127.0.0.1/hello/?arg=8&par=2"	#-vv
