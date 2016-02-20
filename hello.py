96820ea77dd174b55daa90361b0ea1fdc1b3c5c8
kirimaks <kirimaks@yahoo.com>
2016-02-20 15:38:50 +0000

    c6

--- hello.py
+++ hello.py
@@ -1,6 +1,16 @@
 def app(env, make_response):
+
+    #qs = env['QUERY_STRING']
+	
+    #results = str(type(qs.split("&")))
+
+    results = ""
+
+    for arg in env['QUERY_STRING'].split("&"):
+	results += arg + "\n"
+
     headers = (
         ( "Content-type", "text/plain" ),
     )
     make_response("200 OK", headers)
-    return ("Hello world!!")
+    return (results)
