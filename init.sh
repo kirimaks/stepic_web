372881229619b0c622bca6e013b7a10f5d97ab2e
kirimaks <kirimaks@yahoo.com>
2016-02-20 14:09:50 +0000

    link gitconfig

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+ln -s ~/web/gitconfig ~/.gitconfig 
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
