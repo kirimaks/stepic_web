07d484a047a302b8e94a0a7ce58c9ad37c69f598
kirimaks <kirimaks@yahoo.com>
2016-02-20 15:49:43 +0000

    remove default nginx config

--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
+# Git.
 ln -s ~/web/gitconfig ~/.gitconfig 
+
+# Nginx.
+sudo rm /etc/nginx/sites-enabled/default 
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+
+# Gunicorn.
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
