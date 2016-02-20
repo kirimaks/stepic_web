a6751ee434ad213666ebbbe021675939ea6510a4
kirimaks <kirimaks@yahoo.com>
2016-02-20 14:55:49 +0000

    add gunicorn.conf

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 ln -s ~/web/gitconfig ~/.gitconfig 
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn restart
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
