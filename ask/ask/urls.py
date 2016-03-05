428e52204875c98c34ae0a4c5567bbebdad242a7
kirimaks <kirimaks@yahoo.com>
2016-03-05 11:09:42 +0000

    c11

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -5,10 +5,11 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'qa.views.test', 		name='home'),
-    url(r'^login$', 'qa.views.test', 		name='login'),
-    url(r'^ask$', 'qa.views.test', 		name='ask'),
-    url(r'^popular$', 'qa.views.test', 		name='popular'),
-    url(r'^new$', 'qa.views.test', 		name='new'),
+    url(r'^login/$', 'qa.views.test', 		name='login'),
+    url(r'^ask/$', 'qa.views.test', 		name='ask'),
+    url(r'^popular/$', 'qa.views.test',		name='popular'),
+    url(r'^new/$', 'qa.views.test', 		name='new'),
+    url(r'^signup/$', 'qa.views.test', 		name='signup'),
     url(r'^question/(?P<qid>[0-9]+)/$', 'qa.views.test', name='question'),
 
     url(r'^admin/', include(admin.site.urls)),
