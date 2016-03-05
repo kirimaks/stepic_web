a115e2c69353d205891b1880ac176b82a94b6d72
kirimaks <kirimaks@yahoo.com>
2016-03-05 10:49:45 +0000

    Gunicorn + Django

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -4,12 +4,12 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'qa.views.test', 		name='home'),
-    url(r'^login$', 'qa.views.test', 		name='login'),
-    url(r'^ask$', 'qa.views.test', 		name='ask'),
-    url(r'^popular$', 'qa.views.test', 		name='popular'),
-    url(r'^new$', 'qa.views.test', 		name='new'),
-    url(r'^question/P?<qid>$', 'qa.views.test', name='question'),
+    #url(r'^$', 'qa.views.test', 		name='home'),
+    #url(r'^login$', 'qa.views.test', 		name='login'),
+    #url(r'^ask$', 'qa.views.test', 		name='ask'),
+    #url(r'^popular$', 'qa.views.test', 		name='popular'),
+    #url(r'^new$', 'qa.views.test', 		name='new'),
+    #url(r'^question/P?<qid>$', 'qa.views.test', name='question'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
