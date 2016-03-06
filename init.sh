19f9e0c0d0d9d69380e4ac016c5cec161bee0f48
kirimaks <kirimaks@yahoo.com>
2016-03-06 18:02:31 +0000

    init.sh

--- init.sh
+++ init.sh
@@ -2,25 +2,32 @@ clear
 
 # Git.
 echo "\n*** Make gitcongig ***"
-#ln -s ~/web/gitconfig ~/.gitconfig 
 git_config="$HOME/.gitconfig"
-if [ -z $git_config ]; then
-	ln -s $HOME/web/gitconfig $HOME/.gitconfig
+if [ ! -f $git_config ]; then
+	ln -s $HOME/web/gitconfig $git_config
 fi
 
 # Nginx.
 echo "\n*** Configure nginx ***"
-sudo rm /etc/nginx/sites-enabled/default 
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+old_config="/etc/nginx/sites-enabled/default"
+if [ -f $old_config ]; then rm $old_config; fi
+new_config="/etc/nginx/sites-enabled/test.conf"
+if [ ! -f $new_config ]; then
+	sudo ln -s /home/box/web/etc/nginx.conf $new_config 
+fi
 sudo /etc/init.d/nginx restart
 
 # Gunicorn.
 echo "\n*** Configure gunicorn ***"
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+guni_config="/etc/gunicorn.d/test"
+if [ ! -f $guni_config ]; then 
+	sudo ln -s /home/box/web/etc/gunicorn.conf $guni_config
+fi
 sudo /etc/init.d/gunicorn restart
 
 # Mysql
 echo "\n*** Configure Mysql ***"
+sudo /etc/init.d/mysql restart
 echo "\tCreate table"
 sudo mysql -uroot -e "CREATE DATABASE IF NOT EXISTS stepic;"
 echo "\tCreate user"
