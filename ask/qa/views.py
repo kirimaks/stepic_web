f7160cff7cda6e1da4bc64b0fc4955757bcbaa58
kirimaks <kirimaks@yahoo.com>
2016-03-18 09:25:25 +0000

    c16

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts 		import render, get_object_or_404
-from django.http 		import HttpResponse
+from django.http 		import HttpResponse, HttpResponseRedirect
 from qa.models 			import Question
 from django.core.paginator 	import Paginator
 from qa.forms 			import AskForm
--- tests/curl_post_csrf.sh
+++ tests/curl_post_csrf.sh
@@ -1,5 +1,5 @@
 csrf_token="vdb7DGfUfacMLe2e4jQjp01UZKcoDkJa"
-curl 	-X POST\
+curl -vv -X POST\
 	--data "title=bugaga&text=sometext&csrfmiddlewaretoken=${csrf_token}"\
 	--cookie "csrftoken=${csrf_token}"\
 	localhost/ask/
