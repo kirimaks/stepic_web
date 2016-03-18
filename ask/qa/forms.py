0a18b47d5e630759bc106bc86900bb922f695892
kirimaks <kirimaks@yahoo.com>
2016-03-18 09:22:08 +0000

    c15

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from qa.models import Question
+from django.contrib.auth.models import User
 
 class AskForm(forms.Form):
 	title = forms.CharField()
@@ -8,10 +9,12 @@ class AskForm(forms.Form):
 	def save_and_get_url(self):
 		new_qs = Question(
 				text=self.cleaned_data['text'], 
-				title=self.cleaned_data['title'])
+				title=self.cleaned_data['title'],
+				author=User.objects.all()[0]
+		)
 		new_qs.save()
 
-		return "/question/" + news_qs.pk + "/"
+		return "/question/" + str(new_qs.pk) + "/"
 
 
 class AnswerForm(forms.Form):
