eae907b6bdca4f9a3650fc5b1392276e69f263c1
kirimaks <kirimaks@yahoo.com>
2016-03-18 04:41:16 +0000

    best template

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -8,7 +8,7 @@ def test(request, *args, **kwargs):
 	return HttpResponse("OK")
 
 def home(request):
-	questions_list  = Question.objects.all().order_by("added_at")
+	questions_list  = Question.objects.all().order_by("-added_at")
 	page_num 	= request.GET.get("page", 1)
 	paginator 	= Paginator(questions_list, 10)
 	cur_page 	= paginator.page(page_num)
